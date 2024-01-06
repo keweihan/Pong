4686bdb412d9f8bc7ecf94b23c32f33a158c5fe9
Kewei Han <hankevin@umich.edu>
2024-01-06 18:17:59 -0500

    Add rectangle

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
+
 #include <SimpleECS_Core.h>;
+
 #include <iostream>;
+#include <vector>;
 
 using namespace std;
 using namespace SimpleECS;
 
 int main() {
 	cout << "Hello World!" << endl;
-	Game testGame;
+	
+	// Setup game
+	Game game;
+	Scene* startScene = new Scene();
+	game.addScene(startScene);
 
-	testGame.StartGame();
+	// Create Square and add to scene
+	Entity* squareEntity = new Entity();
+	Component* rectComp = new RectangleRenderer(90, 90);
+	squareEntity->addComponent(rectComp);
+	startScene->AddEntity(squareEntity);
+
+	// Start game loop
+	game.startGame();
 }
