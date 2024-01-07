c58f75296c0831949ec144eda53d7a89d099f97f
Kewei Han <hankevin@umich.edu>
2024-01-06 19:52:37 -0500

    moving rectangle

--- main.cpp
+++ main.cpp
@@ -7,6 +7,34 @@
 using namespace std;
 using namespace SimpleECS;
 
+class RectangleMover : public Component {
+	void update() override
+	{
+		if (Input::getKeyDown(KeyCode::KEY_DOWN_ARROW))
+		{
+			entity->transform.posY -= 1;
+		}
+		else if (Input::getKeyDown(KeyCode::KEY_UP_ARROW))
+		{
+			entity->transform.posY += 1;
+		}
+
+		if (Input::getKeyDown(KeyCode::KEY_LEFT_ARROW))
+		{
+			entity->transform.posX -= 1;
+		}
+		else if (Input::getKeyDown(KeyCode::KEY_RIGHT_ARROW))
+		{
+			entity->transform.posX += 1;
+		}
+	}
+
+	void initialize() override
+	{
+
+	}
+};
+
 int main() {
 	cout << "Hello World!" << endl;
 	
@@ -19,6 +47,10 @@ int main() {
 	Entity* squareEntity = new Entity();
 	Component* rectComp = new RectangleRenderer(90, 90);
 	squareEntity->addComponent(rectComp);
+
+	Component* rectMover = new RectangleMover();
+	squareEntity->addComponent(rectMover);
+
 	startScene->AddEntity(squareEntity);
 
 	// Start game loop
