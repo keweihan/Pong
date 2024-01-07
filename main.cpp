01e49e88ee02cb22ee5e7bf3dc7cb8c2761c5be6
Kewei Han <hankevin@umich.edu>
2024-01-07 00:18:07 -0500

    Implement moving and colliding rectangle
    - Use new "Input" class of SimpleECS
    - Use new "Collision" class of SimpleECS

--- main.cpp
+++ main.cpp
@@ -7,6 +7,42 @@
 using namespace std;
 using namespace SimpleECS;
 
+class RectangleMover : public Component {
+	int collisionFrames = 0;
+
+	void update() override
+	{
+		if (Input::getKeyDown(KeyCode::KEY_DOWN_ARROW))
+		{
+			entity->transform.posY -= 1;
+		}
+		else if (Input::getKeyDown(KeyCode::KEY_UP_ARROW))
+		{
+			entity->transform.posY += 1;
+		}
+
+		if (Input::getKeyDown(KeyCode::KEY_LEFT_ARROW))
+		{
+			entity->transform.posX -= 1;
+		}
+		else if (Input::getKeyDown(KeyCode::KEY_RIGHT_ARROW))
+		{
+			entity->transform.posX += 1;
+		}
+	}
+
+	void initialize() override
+	{
+
+	}
+
+	void onCollide(const Collider& collide) override
+	{
+		cout << "Colliding! " << collisionFrames << "th frame." << endl;
+		++collisionFrames;
+	}
+};
+
 int main() {
 	cout << "Hello World!" << endl;
 	
@@ -15,11 +51,28 @@ int main() {
 	Scene* startScene = new Scene();
 	game.addScene(startScene);
 
-	// Create Square and add to scene
+	// Create Controllable Square and add to scene
 	Entity* squareEntity = new Entity();
+	startScene->AddEntity(squareEntity);
+
 	Component* rectComp = new RectangleRenderer(90, 90);
 	squareEntity->addComponent(rectComp);
-	startScene->AddEntity(squareEntity);
+
+	Component* rectMover = new RectangleMover();
+	squareEntity->addComponent(rectMover);
+
+	Component* rectCollider = new BoxCollider(90, 90);
+	squareEntity->addComponent(rectCollider);
+
+	// Create Static Square and add to scene
+	Entity* staticEntity = new Entity();
+	startScene->AddEntity(staticEntity);
+
+	Component* staticComp = new RectangleRenderer(45, 45);
+	staticEntity->addComponent(staticComp);
+
+	Component* staticCollide = new BoxCollider(45, 45);
+	staticEntity->addComponent(staticCollide);
 
 	// Start game loop
 	game.startGame();
